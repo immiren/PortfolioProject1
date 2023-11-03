6f51f3c78f1605d63c8b7c559c0a2cd2066f1352
immiren <immi.renko@gmail.com>
2023-11-03 13:30:18 +0200

    3.11. work

---
+++ BattleCity/Assets/Scripts/MasterTracker.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MasterTracker : MonoBehaviour
+{
+    static MasterTracker instance=null;
+    
+    public static int smallTankDestroyed, fastTankDestroyed, bigTankDestroyed, armoredTankDestroyed;
+    public static int levelNumber;
+    public static int playerLives = 3;
+    public static int playerScore = 0;
+
+    [SerializeField]
+    int smallTankPoints = 100, fastTankPoints = 200, bigTankPoints = 300, armoredTankPoints = 400;
+    public int smallTankPointsWorth { get { return smallTankPoints;}}
+    public int fastTankPointsWorth { get { return fastTankPoints;}}
+    public int bigTankPointsWorth { get { return bigTankPoints;}}
+    public int armoredTankPointsWorth { get { return armoredTankPoints;}}
+
+    void Awake()
+    {
+        if(instance == null) //check for existing instance of mastertracker
+        {
+            DontDestroyOnLoad(gameObject);
+            instance = this;    
+        }else if(instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
