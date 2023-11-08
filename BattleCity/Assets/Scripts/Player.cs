5c3418d8a2f6164d923856df9ef21373e47b1175
immiren <immi.renko@gmail.com>
2023-11-08 13:42:35 +0200

    8.11. Work
    Movement + Enemy AI work, added placeholder graphics

---
+++ BattleCity/Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : Movement
+{
+    Rigidbody2D rb2d;
+    float h, v;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        h = Input.GetAxisRaw("Horizontal");
+        v = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        if (h != 0 && !isMoving) StartCoroutine(MoveHorizontal(h,rb2d));
+        else if (v != 0 && !isMoving) StartCoroutine(MoveVertical(v,rb2d));
+    }
+}
